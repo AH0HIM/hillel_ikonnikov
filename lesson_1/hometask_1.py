4da3fcde3f6c5c26061c7014434c8999fc779325
AH0HIM <i666943097@gmail.com>
2022-01-21 19:41:53 +0200

    lesson 1 passed

---
+++ lesson_1/hometask_1.py
@@ -0,0 +1,9 @@
+x = "*\t\t*\t\t*\t\t*\t\t\t\t\b  *\t *\t*\t\n" \
+    "*\t  *\t*\t  *\t  *\t\t*\t\t\t*\t\t*\n"\
+    "*\t *  *\t*\t\t\t\b*\t*\t\t\t*\t\t*\n"\
+    "*\t*\t*\t*\t\t*\t*\t\t\b*\t*\t  *\t *\t*\t\n"\
+    "*  *\t*\t*\t\t*\t*\t\t *\t  *\t\t*\n"\
+    "* *\t\t*\t*\t\t*\t*\t\t *\t *\t\t*\n"\
+    "*\t\t*\t*\t\t*\t*\t\t\b*\t*\t*\t\t*\n"
+
+print(x)
---
+++ lesson_1/main.py
@@ -0,0 +1,27 @@
+
+
+# x = input("Enter a number: ")
+
+# size = len(x)
+
+some_number = 4 # snake case
+someNumber = 5 # camel case
+SomeNumber = 6
+
+# print(x)
+# print(size)
+
+"""
+*     *
+**    *  
+* *   *
+*  *  *
+*   * *
+*    **
+"""
+
+str = "*\t\t*\n*" \
+      "*\t\t*"
+
+print(str)
+
