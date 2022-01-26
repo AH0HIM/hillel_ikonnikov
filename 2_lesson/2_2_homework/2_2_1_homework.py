64da05779fe9f21dc96194c1eeb1e1e15adbca98
AH0HIM <i666943097@gmail.com>
2022-01-26 18:42:17 +0200

    add calculate hour

--- 2_lesson/2_2_homework/2_2_1_homework.py
+++ 2_lesson/2_2_homework/2_2_1_homework.py
@@ -6,13 +6,23 @@
 n, m = int(input("Сколько километров проезжает машина за день?\n")), \
        int(input("Сколько километров проедет машина по маршруту?\n"))
 
-cal_time = m // n
+cal_day = m // n
+cal_hour = str((m / n) - (m // n)).split(".")[1][0]
 
-if cal_time % 10 == 1 and cal_time % 100 != 11:
+
+if cal_day % 10 == 1 and cal_day % 100 != 11:
     day = "день"
-elif 2 <= cal_time % 10 <= 4 and not 12 <= n % 100 <= 14:
+elif 2 <= cal_day % 10 <= 4 and not 12 <= n % 100 <= 14:
     day = "дня"
 else:
     day = "дней"
 
-print(f"Требуется {cal_time} {day}, чтобы проехать маршрут длинной {m} километров.")
+if int(cal_hour) % 10 == 1:
+    hour = "час"
+elif 2 <= int(cal_hour) % 10 and not 5 <= int(cal_hour) % 100 <= 20:
+    hour = "часа"
+else:
+    hour = "часов"
+
+
+print(f"Требуется {cal_day} {day} и {cal_hour} {hour}, чтобы проехать маршрут длинной {m} километров.")
