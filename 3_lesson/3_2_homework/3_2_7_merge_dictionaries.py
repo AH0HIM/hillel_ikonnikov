7448d7a68b56e7dbd3f5b393e19d674385e0b33c
AH0HIM <i666943097@gmail.com>
2022-02-02 19:14:46 +0200

    update 3_2_7 task

--- 3_lesson/3_2_homework/3_2_7_merge_dictionaries.py
+++ 3_lesson/3_2_homework/3_2_7_merge_dictionaries.py
@@ -10,7 +10,9 @@ dic3={5:50, 6:60}
 dic1 = {1: 10, 2: 20}
 dic2 = {3: 30, 4: 40}
 dic3 = {5: 50, 6: 60}
+my_dict = {}
 
-dic4 = dic1 | dic2 | dic3
+for key in (dic1, dic2, dic3):
+    my_dict.update(key)
 
-print(dic4)
+print(my_dict)
