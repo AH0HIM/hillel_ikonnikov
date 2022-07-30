8b22d94025bdc0e9890cc4abeff6471e71f4fb8b
i.ikonnikov <i.ikonnikov@pin-up.team>
2022-07-30 20:44:45 +0300

    test

--- lesson_20/homeworks/test.py
+++ lesson_20/homeworks/test.py
@@ -58,7 +58,7 @@ def git_commit(commit_message, branch):
 
         else:
             input('push2')
-            push_result = subprocess.run(GitClass.push.format(branch),
+            push_result = subprocess.run('git push',
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE,
                                          encoding='windows-1251')
