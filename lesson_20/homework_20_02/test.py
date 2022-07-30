81a9a51f16b22e1f616e92b63ee0d797c2fbec9d
i.ikonnikov <i.ikonnikov@pin-up.team>
2022-07-30 17:18:12 +0300

    innit commit

--- lesson_20/homework_20_02/test.py
+++ lesson_20/homework_20_02/test.py
@@ -1,23 +1,36 @@
 import subprocess
 
-result = subprocess.run('IPCONFIG',
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        encoding='windows-1251')
 
-print(result.stdout)
+def git_commit():
+    result = subprocess.run('git status',
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            encoding='windows-1251')
+
+    if result.returncode == 0:
+        print(result.stdout)
+        print('OK')
+    else:
+        print(result.stdout + result.stderr)
+        print('Something wrong')
 
 
-
+git_commit()
 
 """
+сделать коммит на гите, 
 git status
 -- if untracked files 
 
 git add .
 
+-- if no error = ok
 
 
+git commit - m '{message}'
 
+-- if no error = ok
+
+git status
 
 """
