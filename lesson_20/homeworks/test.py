c4905dce3347b6a7f05de5c78e583be93e83c9f1
i.ikonnikov <i.ikonnikov@pin-up.team>
2022-07-30 20:01:22 +0300

    asd

--- lesson_20/homeworks/test.py
+++ lesson_20/homeworks/test.py
@@ -1,109 +1,76 @@
 import subprocess
 import argparse
 from dataclasses import dataclass
-from enum import Enum
+from loguru import logger
 
+logger.add("logs/logs.log", format="{time} {level} {message}",
+           level="DEBUG", rotation="10 KB")
 
-# from loguru import logger
-#
-#
-# logger.add("logs/logs.log", format="{time} {level} {message}",
-#            level="DEBUG", rotation="10 KB")
-#
-# logger.info("Changes to be committed")
-# logger.error("Something wrong")
+logger.info("Changes to be committed")
+logger.error("Something wrong")
 
 
 @dataclass(frozen=True)
 class GitClass:
-    STATUS = 'git status'
-    ADD = 'git add .'
-    COMMIT = 'git commit -m {}'
-    PUSH = 'git push {}'
-    OK = 0
-    ERROR = 1
-
-
-changes = 'Changes to be committed'
-
-
-# class ResultCode(Enum):
-#     OK = 0
-#     ERROR = 1
-
-#
-# class GitCommand(Enum):
-#     STATUS = 'git status'
-#     ADD = 'git add .'
-#     COMMIT = 'git commit -m {}'
-#     PUSH = 'git push {}'
+    status = 'git status'
+    add = 'git add .'
+    commit = 'git commit -m {}'
+    push = 'git push {}'
+    ok = 0
+    error = 1
+    changes = 'Changes to be committed'
 
 
 def git_commit(commit_message, branch):
-    status_result = subprocess.run(GitClass.STATUS,
+    status_result = subprocess.run(GitClass.status,
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE,
                                    encoding='windows-1251')
 
-    if status_result.returncode == GitClass.ERROR:
-        print(status_result.stdout + status_result.stderr)
+    if status_result.returncode == GitClass.error:
+        logger.error(status_result.stdout + status_result.stderr)
 
     else:
         input('add')
-        if status_result.stdout.find(changes):
+        if status_result.stdout.find(GitClass.changes):
             print(status_result.stdout)
-            add_result = subprocess.call(GitClass.ADD)
-            if status_result.returncode == GitClass.ERROR:
-                print('error')
+            add_result = subprocess.call(GitClass.add)
+            if status_result.returncode == GitClass.error:
+                logger.error('error')
             else:
                 input('commit')
-                commit_result = subprocess.run(GitClass.COMMIT.format(commit_message),
+                commit_result = subprocess.run(GitClass.commit.format(commit_message),
                                                stdout=subprocess.PIPE,
                                                stderr=subprocess.PIPE,
                                                encoding='windows-1251')
-                if status_result.returncode == GitClass.ERROR:
-                    print(status_result.stdout + status_result.stderr)
+                if status_result.returncode == GitClass.error:
+                    logger.error(commit_result.stdout + commit_result.stderr)
 
                 else:
                     input('push')
-                    push_result = subprocess.run(GitClass.PUSH.format(branch),
+                    push_result = subprocess.run(GitClass.push.format(branch),
                                                  stdout=subprocess.PIPE,
                                                  stderr=subprocess.PIPE,
                                                  encoding='windows-1251')
+                    if status_result.returncode == GitClass.error:
+                        logger.error(push_result.stdout + push_result.stderr)
 
         else:
-            print("Nothing to commit")
+            logger.error("Nothing to commit")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("commit_message")
-    parser.add_argument("branch")
+parser = argparse.ArgumentParser()
+parser.add_argument("--message", type=str, help='input commit message')
+parser.add_argument("--branch", type=str, help='input branch')
+args = parser.parse_args()
 
-    args = parser.parse_args()
+git_commit(args.commit_message, args.branch)
 
-    print(args)
-    git_commit(args.commit_message, args.branch)
-
-"""
-сделать коммит на гите, 
-git status
--- if untracked files 
-
-git add .
-
--- if no error = ok
-
-
-git commit - m '{message}'
-
--- if no error = ok
-
-git status
-
-if nothing to commit, working tree clean
-
-
-git push {origin}
-
-"""
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("commit_message")
+#     parser.add_argument("branch")
+#
+#     args = parser.parse_args()
+#
+#     git_commit(args.commit_message, args.branch)
