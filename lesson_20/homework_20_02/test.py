1befc6018d15e6a7b025bdc31781186c6dfd6635
i.ikonnikov <i.ikonnikov@pin-up.team>
2022-07-30 18:12:12 +0300

    innit

--- lesson_20/homework_20_02/test.py
+++ lesson_20/homework_20_02/test.py
@@ -1,18 +1,65 @@
 import subprocess
+from enum import Enum
+from loguru import logger
+
+logger.debug("")
+logger.info("")
+logger.error("")
+
+
+
+
+changes = 'Changes to be committed'
+message = 'innit'
+branch = 'origin/master'
+
+
+class ResultCode(Enum):
+    OK = 0
+    ERROR = 1
+
+
+class GitCommand(Enum):
+    STATUS = 'git status asd'
+    ADD = 'git add .'
+    COMMIT = 'git commit -m {}'
+    PUSH = 'git push {}'
 
 
 def git_commit():
-    result = subprocess.run('git status',
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            encoding='windows-1251')
+    status_result = subprocess.run(GitCommand.STATUS.value,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   encoding='windows-1251')
 
-    if result.returncode == 0:
-        print(result.stdout)
-        print('OK')
+    if status_result.returncode == ResultCode.ERROR.value:
+        # print(status_result.stdout + status_result.stderr)
+        # print('Something wrong')
+        logger.error(status_result.stdout + status_result.stderr)
     else:
-        print(result.stdout + result.stderr)
-        print('Something wrong')
+        if status_result.stdout.find(changes):
+            print(status_result.stdout)
+            add_result = subprocess.call(GitCommand.ADD.value)
+            if status_result.returncode == ResultCode.ERROR.value:
+                print(status_result.stdout + status_result.stderr)
+                print('Something wrong')
+            else:
+                commit_result = subprocess.run(GitCommand.COMMIT.value.format(message),
+                                               stdout=subprocess.PIPE,
+                                               stderr=subprocess.PIPE,
+                                               encoding='windows-1251')
+                if status_result.returncode == ResultCode.ERROR.value:
+                    print(status_result.stdout + status_result.stderr)
+                    print('Something wrong')
+
+                else:
+                    push_result = subprocess.run(GitCommand.PUSH.value.format(branch),
+                                                 stdout=subprocess.PIPE,
+                                                 stderr=subprocess.PIPE,
+                                                 encoding='windows-1251')
+
+        else:
+            print("Nothing to commit")
 
 
 git_commit()
@@ -32,5 +79,10 @@ git commit - m '{message}'
 -- if no error = ok
 
 git status
+
+if nothing to commit, working tree clean
+
+
+git push {origin}
 
 """
